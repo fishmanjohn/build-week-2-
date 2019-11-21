c9f8da07dd7c0e05679b980ea012afc99f0d36ed
fishmanjohn <john@emmaandjohnbyvan.com>
2019-11-21 14:57:23 -0800

    did some simple styling

--- rv-front-end/src/components/CatalogCard.js
+++ rv-front-end/src/components/CatalogCard.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import {BrowserRouter as Router,Link,Route} from 'react-router-dom';
 import PropertyDetails from './PropertyDetails'
+import styled from 'styled-components'
 
+const StyledImg = styled.img`
+height: 250px; 
+margin: 15px;
+`
 
 const CatalogCard = (props) => {
     const camp = props.data
     return (
       <div>
-         <img alt='' src ={camp.img_url}/>
+         <StyledImg alt='' src ={camp.img_url}/>
          <h1>{camp.name}</h1>
           <h2>{camp.address}</h2>
           <p>{camp.description}</p>
           
-          <Link to = '/PropertyDetails'>View This Property</Link>
-        <Route path = '/PropertyDetails'  render= {(props)=><PropertyDetails {...props} camp={camp} />} />
+          
     
       </div>
     )
--- rv-front-end/src/components/Navbar.js
+++ rv-front-end/src/components/Navbar.js
@@ -2,13 +2,24 @@ import React from 'react';
 import {BrowserRouter as Router,Link,Route} from 'react-router-dom';
 import Catalog from './Catalog';
 import OwnerPropertys from './OwnerProperties';
+import styled from 'styled-components';
+
+const FlexDiv = styled.div`
+display:flex;
+justify-content:space-evenly;
+text-decoration:none;
+color:black;
+`
+
 const Navbar = () => {
   return (
     <div className = 'navbar'>
       <Router>
-        <h1>RV Air B and B.</h1>
+        <h1>Unit 2 Build Demonstaration (not Rv air BnB)</h1>
+        <FlexDiv>
         <Link to = '/Catalog'>Home</Link>
-        <Link to='/OwnerPropertys' >Create Listing</Link>
+        <Link to='/OwnerPropertys' >Get a joke...</Link>
+        </FlexDiv>
         <Route path = '/Catalog' component = {Catalog}/>
         
        <Route path ='/Ownerpropertys' component = {OwnerPropertys}/> 
